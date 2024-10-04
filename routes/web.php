f3209ae8ea4b5fc60e5a9231da4a2ad8b9db7886
adstprmdta <adistypramudita03@gmail.com>
2024-10-04 10:08:50 +0700

    tata pabrik

--- routes/web.php
+++ routes/web.php
@@ -283,3 +283,35 @@ Route::get('/distributor/rekening', function () {
     return view('distributor.rekening');
 })->name('distributor-rekening');
 Route::get('/distributor/pengaturan-bank/{idUser}', [PengaturanBankDistributorController::class, 'index'])->name('pengaturanBankDistributor');
+
+// pabrik
+Route::get('/pabrik/login', function () {
+    return view('pabrik.login');
+})->name('login-pabrik');
+
+Route::post('/pabrik/login', function () {
+    $username = request('username');
+    $password = request('password');
+
+    if ($username === 'pabrik' && $password === '123') {
+        return redirect()->route('dashboard-pabrik');
+    }
+
+    return redirect()->route('login-pabrik')->withErrors(['login' => 'Username atau password salah.']);
+})->name('login-post');
+
+Route::get('/pabrik/dashboard', function () {
+    return view('pabrik.dashboard');
+})->name('dashboard-pabrik');
+
+Route::get('/pabrik/distributor', function () {
+    return view('pabrik.kelola-akun');
+})->name('kelola-pabrik');
+
+Route::get('/pabrik/laporan', function () {
+    return view('pabrik.laporan');
+})->name('laporan-pabrik');
+
+Route::get('/pabrik/detail-laporan', function () {
+    return view('pabrik.detail-laporan');
+})->name('detailLaporan-pabrik');
